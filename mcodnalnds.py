bf8487d5cfa49759a3c20f00b4f155cdf3dbdfd3
boqyo <144711478+boqyo@users.noreply.github.com>
2023-11-01 20:19:09 -0700

    Create mcodnalnds.py

---
+++ mcodnalnds.py
@@ -0,0 +1,23 @@
+# Author: Shaun
+# date: nov 1 2023
+# mcdndalds order and tax
+
+order_cost = 0
+
+burger = int(5)
+fries = int(3)
+
+# ask if they want burger fries
+# if they want burger fry then add price
+
+burger_ask = input("Do you want burger for 5$ (yes/no) ")
+if burger_ask.lower().strip("!,.?") == "yes":
+    order_cost += int(5)
+
+fries_ask = input("Do you want fries 3$ (yes/no) ")
+if fries_ask.lower().strip("!,.?") == "yes":
+    order_cost += int(3)
+
+# add tax
+print(order_cost * 1.14)
+
