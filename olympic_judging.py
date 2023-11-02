3a8f6efd11400c9a597b2a283fb84d3120b322f8
boqyo <144711478+boqyo@users.noreply.github.com>
2023-11-01 19:55:25 -0700

    Update olympic_judging.py

--- olympic_judging.py
+++ olympic_judging.py
@@ -4,12 +4,10 @@ judge_score = ["Judge 1: ", "Judge 2: ", "Judge 3: ", "Judge 4: ", "Judge 5: "]
 
 for judge in judge_score:
     print(judge)
-    score = int(input().strip("!,.?"))
-    final_score += score
+    score = float(input().strip("!,.?"))
+    if score < 0 or score > 10:
+        break
+    else:
+        final_score += score
 
-
-
-if score < 0 or score > 10:
-    
-
-print(final_score) / 5
+print({round ((final_score / 5), 2)})
