8664e4bf408991ee9ddee09f774203a1c5ba41f3
Shaun HA <shaun16.ha@gmail.com>
2023-11-06 09:00:51 -0800

    Finished projects

--- semester_evaluator.py
+++ semester_evaluator.py
@@ -4,6 +4,19 @@
 
 total_rating = 0
 
-course_amount = input(f"How many courses are you taking? {cl}: ")
+course_amount = int(input(f"How many courses are you taking? "))
 
+for i in range(course_amount):
+    rating = int(input(f"How do you rate course {i + 1}? "))
+    total_rating += rating
 
+average = total_rating / course_amount
+
+if average <= 1:
+    print(average, "Ouch.")
+
+if average > 1 and average <= 3:
+    print(average, "Not bad.")
+
+if average > 3 and average <= 5:
+    print(average ,"Glad to hear")
