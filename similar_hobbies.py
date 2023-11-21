f5ba092853f0bba776b07f04c6931b7395625654
Shaun HA <shaun16.ha@gmail.com>
2023-11-21 08:30:18 -0800

    found the mistake

--- similar_hobbies.py
+++ similar_hobbies.py
@@ -1,16 +1,16 @@
 # Author shuaun
 # nov 15 2023
 
-hobby_same = 0
+
 print("Please enter hobbies seperated by commas and a space.")
 # Get person 1 hobby
-hobby_one = input("Person 1: ").lower().split(",")
+hobby_one = input("Person 1: ").lower().split(", ")
 
 # Get person 2 hobby
-hobby_two = input("Person 2: ").lower().split(",")
+hobby_two = input("Person 2: ").lower().split(", ")
 
 # FInd in common
-
+hobby_same = 0
 for hobbies in hobby_one:
     if hobbies in hobby_two:
         hobby_same += 1
